cec4620800e79040d286a823df7067b5bd9f9999
regimelab <114866071+regimelab@users.noreply.github.com>
2024-02-01 13:05:09 -0800

    Create bartlett.py

---
+++ model/bartlett.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def bartletts_method(data, segment_len):
+    ''' https://en.wikipedia.org/wiki/Bartlett%27s_method
+    A method of averaging periodograms across the data.
+    '''
+    # data length and number of segments
+    k = len(data) // segment_len 
+  
+    # reshape into segments of length segment_len
+    segments = np.reshape(data[:k * segment_len], (k, segment_len))
+  
+    # periodogram for each segment then averaged 
+    periodograms = np.abs(np.fft.fft(segments, axis=1)) ** 2 / segment_len
+    return np.mean(periodograms, axis=0)
+
+
+def frequency_diagram(signal, segment_len=64):
+
+  # show raw signal 
+  plt.plot(signal)
+  plt.show()
+  
+  # get result
+  result = bartletts_method(signal, segment_len)
+  
+  # return result with bins
+  freq_bins = np.fft.fftfreq(segment_len)[:segment_len//2]
+  return freq_bins, result
